86d27e0384c16a42066d2d9e9f57e2eb56f08268
Jake Golle <jake.mmg@gmail.com>
2026-04-06 20:59:37 -0400

    Haversine distance formula completed

--- location-scouting-web-application/src/app/services/haversineService.ts
+++ location-scouting-web-application/src/app/services/haversineService.ts
@@ -1,3 +1,8 @@
+export const DEFAULT_MAX_DISTANCE_KM = 100
+export const DEFAULT_MIN_DISTANCE_KM = 0.3
+export const MAX_PROXIMITY_SCORE = 1.0
+export const MIN_PROXIMITY_SCORE = 0
+
 export function haversineDistance(lat1: number, lng1: number, lat2: number, lng2: number) : number {
     // distance between lats and longs
     let dLat : number = (lat2 - lat1) * Math.PI / 180.0
@@ -15,4 +20,15 @@ export function haversineDistance(lat1: number, lng1: number, lat2: number, lng2
     let rad : number = 6371;
     let c : number = 2 * Math.asin(Math.sqrt(a))
     return rad * c;
+}
+
+export function calculateProximityScore(distance: number, maxDistance?: number) : number {
+    maxDistance = maxDistance || DEFAULT_MAX_DISTANCE_KM;
+
+    // outside the range? Clamp it
+    if(distance < DEFAULT_MIN_DISTANCE_KM) return 1
+    if(distance > DEFAULT_MAX_DISTANCE_KM) return 0
+
+
+    return Math.round(((maxDistance - distance) / (maxDistance - DEFAULT_MIN_DISTANCE_KM)) * 100) / 100;
 }
--- location-scouting-web-application/src/test/haversineService.test.ts
+++ location-scouting-web-application/src/test/haversineService.test.ts
@@ -1,6 +1,10 @@
 import {it, describe, expect} from "vitest";
-import {haversineDistance} from "@/app/services/haversineService";
-
+import {
+    calculateProximityScore,
+    DEFAULT_MAX_DISTANCE_KM, DEFAULT_MIN_DISTANCE_KM,
+    haversineDistance,
+    MAX_PROXIMITY_SCORE, MIN_PROXIMITY_SCORE
+} from "@/app/services/haversineService";
 
 describe('HaversineService', () => {
     // Used for reference/source-of-truth: https://www.distancecalculator.net/from-toronto-to-ottawa
@@ -21,11 +25,53 @@ describe('HaversineService', () => {
             lat2: 43.651070, lng2: -79.347015,
             expected: 0
         },
+        {
+            from: 'Toronto',
+            to: 'Toronto\'s Antipodal Point',
+            lat1: 43.651070, lng1: -79.3470,
+            lat2: -43.651070, lng2: 100.6530,
+            expected: 20015
+        }
     ])(' should correctly calculate the distance between $from known $to', ({ lat1, lng1, lat2, lng2, expected }) =>{
         const result = haversineDistance(lat1, lng1, lat2, lng2)
         console.log(result)
         expect(result).toBeCloseTo(expected, -1) // within -/+ 5km
     })
 
+    describe('Proximity Score Calculation', () =>{
+        it.each([
+            {
+                // Minimum (0.3 currently or 300 meters. This is walking distance, essentially)
+                distance: DEFAULT_MIN_DISTANCE_KM,
+                expected: MAX_PROXIMITY_SCORE
+            },
+            {
+                // Halfway
+                distance: (DEFAULT_MAX_DISTANCE_KM + DEFAULT_MIN_DISTANCE_KM) / 2,
+                expected: 0.5
+            },
+            {
+                // Maximum (100 km currently)
+                distance: DEFAULT_MAX_DISTANCE_KM,
+                expected: MIN_PROXIMITY_SCORE
+            },
+            {
+                // Over Maximum
+                distance: DEFAULT_MAX_DISTANCE_KM + 0.01,
+                expected: MIN_PROXIMITY_SCORE
+            },
+            {
+                // Under Minimum
+                distance: DEFAULT_MIN_DISTANCE_KM - 0.01,
+                expected: MAX_PROXIMITY_SCORE
+            }
+        ])(' should score $expected on location within $distance km', ({distance, expected}) => {
+            // Act
+            const result : number = calculateProximityScore(distance)
 
+            // Assert
+            expect(result).toBeDefined()
+            expect(result).toEqual(expected)
+        })
+    })
 })
