5408e0161ee5413c58ce1f6f8816ca3c96def634
Jake Golle <jake.mmg@gmail.com>
2026-09-14 10:26:29 -0400

    Added more test IDs to the authenticated header for the user name, email and the signout button

--- location-scouting-web-application/src/test/e2e/pom/components/authenticated-header.ts
+++ location-scouting-web-application/src/test/e2e/pom/components/authenticated-header.ts
@@ -5,10 +5,16 @@ export class AuthenticatedHeader {
     readonly page: Page;
     readonly shell: Locator;
     readonly userMenuButton: Locator;
+    readonly userMenuName: Locator;
+    readonly userMenuEmail: Locator;
+    readonly signOutButton: Locator;
 
     constructor (page: Page) {
         this.page = page;
         this.shell = page.getByTestId('authenticated-shell');
         this.userMenuButton = page.getByTestId('user-menu-button');
+        this.userMenuName = page.getByTestId('user-menu-name');
+        this.userMenuEmail = page.getByTestId('user-menu-email');
+        this.signOutButton = page.getByTestId('user-menu-signout');
     }
 }
